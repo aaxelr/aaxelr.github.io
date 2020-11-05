9a80e456e6384720cb477a9a34709efa082cd37e
Antonio A R <antonio.axelsson@gmail.com>
2020-11-05 17:50:02 +0100

    redid class components and methods. Added functionality to show one view at a time.

--- js/main.js
+++ js/main.js
@@ -1,33 +1,42 @@
 class Spa {
   constructor() {
-    this.views = Array.from(document.getElementsByTagName("section"));
-    this.menu = {
+    this.views = {
+      home: document.getElementById("home"),
+      portfolio: document.getElementById("portfolio"),
+      about: document.getElementById("about"),
+      contact: document.getElementById("contact"),
+    }
+    this.links = {
       home: document.getElementById("home-link"),
       portfolio: document.getElementById("portfolio-link"),
       about: document.getElementById("about-link"),
       contact: document.getElementById("contact-link"),
-    }
+    };
   }
 
-  setupMenu() {
-    for (let link in this.menu) {
-      this.menu[link].addEventListener("click", function () {
-        console.log(`You clicked on ${link}`);
-        console.log(this.id);
+  setupLinks() {
+    for (let link in this.links) {
+      this.links[link].addEventListener("click", (e) => {
+        let linkID = e.target.id;
+        let viewToShow = linkID.slice(0, -5);
+        this.showView(viewToShow);
       });
     }
   }
 
-  showView(clickedView = "home-link") {
-    this.views.forEach((view) => {
-      view.classList.add("hidden");
-    });
+  showView(receivedView = "home") {
+    for (let view in this.views) {
+      this.views[view].classList.add("hidden");
+      if (receivedView === view) {
+        this.views[view].classList.remove("hidden");
+      }
+    }
   }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Final Portfolio VII...");
+  console.log("A A R ©MMXX");
   const mySpa = new Spa();
-  mySpa.setupMenu();
-
+  mySpa.setupLinks();
 });
